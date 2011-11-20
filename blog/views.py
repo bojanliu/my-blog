8dc5930b1c5213b7727e08c5462c13a0795afd36
BojanLiu <lllhhw@gmail.com>
2011-11-21 00:31:46 +0800

    增加了分页功能

--- blog/views.py
+++ blog/views.py
@@ -45,3 +45,25 @@ def post_by_id(request,post_id):
                                      object_id=post_id,
                                      queryset=Post.objects.filter(published=True),
                                      )    
+
+from django.core.paginator import PageNotAnInteger, Paginator, InvalidPage, EmptyPage
+def post_list(request):
+    posts=Post.objects.all()
+    after_range_num=5 #当前页前显示5页
+    befor_range_num=4 #当前页后显示4页
+    try:  #如果请求的页码少于1或者类型错误，则跳转到第1页
+        page=int(request.GET.get('page',1))#默认显示第一页
+        if page<1:
+            page=1
+    except ValueError:
+        page=1
+    paginator=Paginator(posts,2) # 设置post在每页显示的数量，这里为1
+    try: #跳转到请求页面，如果该页不存在或者超过则跳转到尾页
+        post_list=paginator.page(page)
+    except(EmptyPage, InvalidPage, PageNotAnInteger):
+        post_list=paginator.page(paginator.num_pages)
+    if page >= after_range_num:
+        page_range=paginator.page_range[page-after_range_num:page+befor_page_num]
+    else:
+        page_range=paginator.page_range[0:int(page)+befor_range_num]
+    return render_to_response('blog/post_list.html',{'posts':post_list,'page_range':page_range},)
