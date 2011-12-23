b2a610e866ba12147dd51886c2ebed0fc535099f
bojanliu <lllhhw@gmail.com>
2011-12-23 22:15:53 +0800

    重写了分页机制

--- blog/urls.py
+++ blog/urls.py
@@ -1,27 +1,19 @@
 from django.conf.urls.defaults import *
-from django.views.generic import list_detail,date_based
 from myblog.blog.models import *
 from myblog.blog.views import *
 from django.shortcuts import get_object_or_404
 from myblog.blog.feeds import LatestPosts
 from django.views.decorators.cache import cache_page
 
-archive_info={'month_format':'%m',
-              'date_field':'pub_date',
-              'template_name':'blog/post_list.html',
-              'template_object_name':'post',
-              'queryset':Post.objects.filter(published=True),
-    }
 
 feeds={'latest':LatestPosts,}
 
 urlpatterns = patterns('',
-    url(r'^$',post_list),
-    url(r'^page/(?P<page_id>\d+)/$',post_list,name='post_by_page'),
+    url(r'^$',posts_by_page,name='posts_by_page'),
     url(r'^search/$',search,name='post_search'),
     url(r'^category/(?P<category_id>\d+)/$',posts_by_category,name='posts_by_category'),
     url(r'^tag/(?P<tag_id>\d+)/$',posts_by_tag,name='posts_by_tag'),
-    url(r'^archive/(?P<year>\d{4})/(?P<month>\d{1,2})/$',date_based.archive_month,archive_info,name='posts_by_month'),
+    url(r'^archive/(?P<year>\d{4})/(?P<month>\d{1,2})/$',posts_by_month,name='posts_by_month'),
     url(r'^post/(?P<post_id>\d+)/$',post_by_id,name='post_by_id'),
     url(r'^feeds/(?P<url>.*)/$','django.contrib.syndication.views.feed',{'feed_dict':feeds}),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 # Create your views here.
-#-*- coding:utf-8 -*-
+#-*-coding:utf-8-*-
 from django.shortcuts import render_to_response,get_object_or_404,redirect
 from myblog.blog.models import *
-from django.views.generic import list_detail
+from django.views.generic import list_detail,date_based
 from django.db.models import Q
 
 def search(request):
@@ -21,8 +21,6 @@ def search(request):
     else:
         return render_to_response('search/invalid_search.html')
 
-
-
 def posts_by_category(request,category_id):
     category=get_object_or_404(Category,id=category_id)
     queryset=category.post_set.filter(published=True)
@@ -30,8 +28,8 @@ def posts_by_category(request,category_id):
                                    template_object_name='post',
                                    template_name='blog/post_list.html',
                                    queryset=queryset,
+                                   paginate_by=5,
                                    )
-
 
 def posts_by_tag(request,tag_id):
     tag=get_object_or_404(Tag,id=tag_id)
@@ -40,7 +38,31 @@ def posts_by_tag(request,tag_id):
                                    template_object_name='post',
                                    template_name='blog/post_list.html',
                                    queryset=queryset,
+                                   paginate_by=5,
                                    )
+
+def posts_by_page(request):
+    queryset=Post.objects.all()
+    return list_detail.object_list(
+                     request,
+                     template_object_name='post',
+                     template_name='blog/post_list.html',
+                     queryset=queryset,
+                     paginate_by=5,
+                     )
+
+def posts_by_month(request,year,month):
+    queryset=Post.objects.filter(published=True)
+    return date_based.archive_month(request,
+                                    month_format='%m',
+                                    date_field='pub_date',
+                                    year=year,
+                                    month=month,
+                                    template_name='blog/post_list.html',
+                                    template_object_name='post',
+                                    queryset=queryset,
+                                    extra_context={'is_paginated':False}
+                                    )
 
 def post_by_id(request,post_id):
     return list_detail.object_detail(request,
@@ -48,26 +70,4 @@ def post_by_id(request,post_id):
                                      template_name='blog/post_detail.html',
                                      object_id=post_id,
                                      queryset=Post.objects.filter(published=True),
-                                     )    
-
-from django.core.paginator import PageNotAnInteger, Paginator, InvalidPage, EmptyPage
-def post_list(request,page_id='1'):#缺省视图参数
-    posts=Post.objects.all()
-    after_range_num=5 #当前页前显示5页
-    befor_range_num=4 #当前页后显示4页
-    try:  #如果请求的页码少于1或者类型错误，则跳转到第1页
-        page=int(page_id)
-        if page<1:
-            page=1
-    except ValueError:
-        page=1
-    paginator=Paginator(posts,2) # 设置post在每页显示的数量，这里为1
-    try: #跳转到请求页面，如果该页不存在或者超过则跳转到尾页
-        post_list=paginator.page(page)
-    except(EmptyPage, InvalidPage, PageNotAnInteger):
-        post_list=paginator.page(paginator.num_pages)
-    if page >= after_range_num:
-        page_range=paginator.page_range[page-after_range_num:page+befor_page_num]
-    else:
-        page_range=paginator.page_range[0:int(page)+befor_range_num]
-    return render_to_response('blog/post_list_paginate.html',{'posts':post_list,'page_range':page_range},)
+                                     )
